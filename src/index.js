7d92900f9149ec392e870147e3ea9db3b36c8146
Luan Carlos Araldi <luan.araldi@gmail.com>
2017-05-27 23:58:10 -0300

    Bugfixed when already there is a cookie created

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import axios from 'axios'
 exports.send = function () {
     const url = window.location.href
     let contact_id = document.cookie
+
     if (!contact_id) {
         const randomId = Math.floor(Math.random() * 100000000) + 1
         document.cookie = "contact_id=" + randomId
         contact_id = randomId
+    }else{
+        const idPosition = contact_id.indexOf('=')
+        contact_id = contact_id.substr(idPosition + 1)
     }
     
     axios.post('https://rails-contact-tracking.herokuapp.com/accesses', {
